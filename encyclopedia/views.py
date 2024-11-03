64c4601fc5b2e5e1fd9844a3e034ef4ecb9619e4
Yankey N'wollé Ange Christian <168791054+Yankeyange@users.noreply.github.com>
2024-11-03 23:17:38 +0000

    Add files via upload

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 
+
 from . import util
 
 
@@ -7,4 +8,13 @@ def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
+
+# les détails de chaque entries ou sujet 
+
+
+def entrie(request, entries_id):
+    entrie = Topic.objects.get(id=entries_id)
+    topic = entrie.topics_set.all()
+    context = {'entrie': entrie, "topic":topic}
+    return render(request, "encyclopedia/entrie.html", context)
 
